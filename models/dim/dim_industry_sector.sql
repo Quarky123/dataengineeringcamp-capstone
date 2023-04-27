9269c8de60b2b36e4d21d525d656b136ed42172d
quark <dennis301295@gmail.com>
2023-04-27 13:01:06 +0000

    yaml

---
+++ models/dim/dim_industry_sector.sql
@@ -0,0 +1,18 @@
+-- dim_industry_sector.sql
+
+SELECT
+  'AAPL' as symbol,
+  'Technology' as sector
+UNION ALL
+SELECT
+  'MSFT' as symbol,
+  'Technology' as sector
+UNION ALL
+SELECT
+  'SPY' as symbol,
+  'ETF' as sector
+UNION ALL
+SELECT
+  'TSLA' as symbol,
+  'Automotive' as sector
+
