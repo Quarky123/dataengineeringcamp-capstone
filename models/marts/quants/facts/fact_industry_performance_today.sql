1877c6c73348a364915f225678b7b68cc2da1421
quark <dennis301295@gmail.com>
2023-05-01 13:57:57 +0000

    final

--- models/marts/quants/facts/fact_industry_performance_today.sql
+++ models/marts/quants/facts/fact_industry_performance_today.sql
@@ -1,15 +1,40 @@
-WITH fact_industry_performance_today AS (
+WITH daily_data AS (
     SELECT
-        surrogate_key,
-        -- Include any additional columns you need from the dim_industry_performance_today table
-    FROM {{ ref('dim_industry_performance_today') }}
+        DATE,
+        INDUSTRY_SECTOR,
+        SUM(RETURNS) AS industry_total_returns,
+        COUNT(DISTINCT COMPANY_NAME) AS number_of_companies
+    FROM {{ ref('daily_returns_industry') }}
+    GROUP BY DATE, INDUSTRY_SECTOR
+),
+industry_weighted_average_return AS (
+    SELECT
+        DATE,
+        INDUSTRY_SECTOR,
+        industry_total_returns / number_of_companies AS weighted_avg_return
+    FROM daily_data
+),
+industry_rankings AS (
+    SELECT
+        DATE,
+        INDUSTRY_SECTOR,
+        weighted_avg_return,
+        DENSE_RANK() OVER (PARTITION BY DATE ORDER BY weighted_avg_return DESC) AS industry_rank
+    FROM industry_weighted_average_return
+),
+fact_with_surrogate_key AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['DATE', 'INDUSTRY_SECTOR']) }} AS surrogate_key,
+        DATE,
+        INDUSTRY_SECTOR,
+        weighted_avg_return,
+        industry_rank
+    FROM industry_rankings
 )
-
-
--- Your main query goes here, using the fact_industry_performance_today CTE
 SELECT
-    fact_industry_performance_today.surrogate_key,
-    -- Include any additional columns or calculations you need
-FROM fact_industry_performance_today
--- Add any necessary JOINs or additional CTEs
-
+    surrogate_key AS surrogate_key,
+    DATE AS date,
+    INDUSTRY_SECTOR AS industry_sector,
+    weighted_avg_return AS weighted_average_return,
+    industry_rank AS ranking_of_industry
+FROM fact_with_surrogate_key
